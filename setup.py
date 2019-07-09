c039943869d3a2cd62c4b6ff759857d3b5e7054e
Jacob Duvall <jdaleduvall@gmail.com>
2019-07-09 15:04:36 -0500

    distribution tool

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='palindrome',
+    version='1.0',
+    py_modules=['palindrome'],
+
+    # metadata
+    author='Jacob Duvall',
+    author_email='jdaleduvall@gmail.com',
+    description='A module for finding palindromic numbers.',
+    license='public domain',
+    keywords='example'
+
+)
